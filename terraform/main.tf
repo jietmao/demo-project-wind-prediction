bfaaeafb6d7841524c3667416a1c077cf2002849
jietmao <jietmao@gmail.com>
2024-04-23 12:16:24 +0800

    Added terraform folder

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  profile     = "localstack"
+  region      = "eu-west-1"
+  access_key  = "abc"
+  secret_key  = "xyz"
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3             = "http://s3.localhost.localstack.cloud:4566"
+  }
+}
+
+resource "aws_s3_bucket" "project_bucket" {
+  bucket = "project-bucket"
+}
